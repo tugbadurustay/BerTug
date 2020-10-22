b648209a368485548e716e84daf8eb6a33b131b5
Tugba Durustay (BilgeAdam Ogrenci) <tugba.durustay@baogrenci.com>
2020-10-22 11:01:25 +0300

    Proje dosyası ekle.

---
+++ BerTug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerTug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellooooo :)");
+            Console.ReadKey();
+        }
+    }
+}
